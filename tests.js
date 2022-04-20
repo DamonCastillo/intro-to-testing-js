35c1efb76d50c02445526597547d7b67ab630849
Damon Castillo <damono7.castillo@gmail.com>
2022-04-20 14:35:34 -0500

    add the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -35,6 +35,9 @@ describe('sayHello', function (){
     it('should be a defined function ', function () {
        expect(typeof sayHello).toBe("function")
     });
+    it('should return a string when when called ', function () {
+        expect(typeof sayHello()).toBe("string")
+    });
     it('should ', function () {
         
     });
