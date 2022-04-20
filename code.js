93d71c2c2d6cb0de82121a980dd565e1b8493c25
Damon Castillo <damono7.castillo@gmail.com>
2022-04-20 14:30:23 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,12 @@
 function helloWorld() {
     return "Hello, World!";
 }
+function plusOne(num){
+    if (typeof num === "number" ){
+        return num + 1;
+    }
+    return 0;
+}
+function sayHello(){
+    return "sayHello"
+}
